dfa48394d819f42b54a78c019960fa3880ee5199
jbellue <julien@bellue.fr>
2021-07-24 18:33:34 +0200

    update serviceWorker to try and debug it

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,36 +1,64 @@
+const cacheName = "elektu-pwa-v0.9"
 const filesToCache = [
-  "/elektu/index.html",
-  "/elektu/style.min.css",
-  "/elektu/elektu.min.js",
-  "/elektu/img/fullscreen-checked.svg",
-  "/elektu/img/fullscreen-unchecked.svg",
-  "/elektu/img/group-checked.svg",
-  "/elektu/img/group-unchecked.svg",
-  "/elektu/img/help-checked.svg",
-  "/elektu/img/help-unchecked.svg",
-  "/elektu/img/ordinate-checked.svg",
-  "/elektu/img/ordinate-unchecked.svg",
-  "/elektu/img/select-checked.svg",
-  "/elektu/img/select-unchecked.svg",
-  "/elektu/img/vibration-checked.svg",
-  "/elektu/img/vibration-unchecked.svg",
-  "/elektu/img/GitHub-Mark-Light-32px.png"
+  "/index.html",
+  "/style.min.css",
+  "/elektu.min.js",
+  "/img/fullscreen-checked.svg",
+  "/img/fullscreen-unchecked.svg",
+  "/img/group-checked.svg",
+  "/img/group-unchecked.svg",
+  "/img/help-checked.svg",
+  "/img/help-unchecked.svg",
+  "/img/ordinate-checked.svg",
+  "/img/ordinate-unchecked.svg",
+  "/img/select-checked.svg",
+  "/img/select-unchecked.svg",
+  "/img/vibration-checked.svg",
+  "/img/vibration-unchecked.svg",
+  "/img/GitHub-Mark-Light-32px.png"
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener("install", function(e) {
-    e.waitUntil(
-        caches.open("elektu-pwa").then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(filesToCache);
+    })());
 });
 
 /* Serve cached content when offline */
-self.addEventListener("fetch", function(e) {
+self.addEventListener("fetch", (e) => {
     e.respondWith(
         caches.match(e.request).then(function(response) {
             return response || fetch(e.request);
         })
     );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) {
+            return r;
+        }
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
+});
+
+self.addEventListener("activate", (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+        Promise.all(keyList.map((key) => {
+            if (key === cacheName) {
+                return;
+            }
+            caches.delete(key);
+        }))
+    })());
 });
