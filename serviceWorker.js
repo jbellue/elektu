72047e85023ecdd8946dc808d7d8a74699f0bf97
jbellue <julien@bellue.fr>
2021-07-24 18:49:11 +0200

    lint serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "elektu-pwa-v0.9.2"
+const cacheName = "elektu-pwa-v0.9.3";
 const filesToCache = [
   "/elektu/index.html",
   "/elektu/style.min.css",
@@ -19,11 +19,10 @@ const filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+self.addEventListener("install", (e) => {
     e.waitUntil((async () => {
+        /* eslint-disable-next-line security/detect-non-literal-fs-filename -- Safe as no value holds user input */
         const cache = await caches.open(cacheName);
-        console.log('[Service Worker] Caching all: app shell and content');
         await cache.addAll(filesToCache);
     })());
 });
@@ -37,16 +36,14 @@ self.addEventListener("fetch", (e) => {
     );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
     e.respondWith((async () => {
         const r = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
         if (r) {
             return r;
         }
         const response = await fetch(e.request);
         const cache = await caches.open(cacheName);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
         cache.put(e.request, response.clone());
         return response;
     })());
@@ -59,6 +56,6 @@ self.addEventListener("activate", (e) => {
                 return;
             }
             caches.delete(key);
-        }))
+        }));
     })());
 });
